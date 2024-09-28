3abb99ec4de48239e4e00faecb71490a5ccb9de8
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-09-28 18:39:52 -0500

    fixed image

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -4,8 +4,8 @@ const Hero = () =>{
   return(
     <>
       <picture id="hero">
-        <source srcSet="./images/hero-desktop.jpg" media="(min-width: 64rem)" />
-        <img className={`${composition_img} ${composition_img_xl}`} src="./images/hero-mobile.jpg" alt="Young woman with wavy brown hair in a coral button-up shirt, standing in front of large green leaves and a pinkish-red wall." loading="lazy" decoding="async"/>
+        <source srcSet="./images/hero-desktop.avif" media="(min-width: 64rem)" />
+        <img className={`${composition_img} ${composition_img_xl}`} src="./images/hero-mobile.avif" alt="Young woman with wavy brown hair in a coral button-up shirt, standing in front of large green leaves and a pinkish-red wall." loading="lazy" decoding="async"/>
       </picture>
     </>
   )
